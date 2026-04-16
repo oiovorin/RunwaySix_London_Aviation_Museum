2a3f7b6883c0cdb4e6dba7d9974f18fce88c3031
Alex4747-J <npanh1903@gmail.com>
2026-04-16 02:37:58 -0400

    Update the Styling and JS
    Update the styling for header nav and also revert the js rule back to original since there's no need for a hamburger menu

--- js/modules/header.js
+++ js/modules/header.js
@@ -4,7 +4,7 @@ const aboutMenu = document.querySelector("#about-nav");
 const historyMenu = document.querySelector("#header-nav");
 
 function mobMenuOpen () {
-    if(hamburger.classList.contains("active") && historyMenu.classList.contains("active") && (!aboutMenu || aboutMenu.classList.contains("active"))) {
+    if(hamburger.classList.contains("active") && historyMenu.classList.contains && aboutMenu.classList.contains("active")) {
         hamburger.classList.remove("active");
         historyMenu.classList.remove("active");
         aboutMenu.classList.remove("active")
