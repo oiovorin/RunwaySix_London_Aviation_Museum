02e25a4e137dbc0a3b35449ccb75ec5ce932221a
Alex4747-J <npanh1903@gmail.com>
2026-04-22 00:38:15 -0400

    Update: Contact Form to Backend
    Connect the contact form to backend

--- js/modules/contact.js
+++ js/modules/contact.js
@@ -1,55 +1,73 @@
 export function form () {
     const emptyForm = function() {
-    return {
-        fullName: '',
-        email: '',
-        subject: '',
-        message: '',
-        honeypot: ''
-    }
-}
-
-const app = Vue.createApp({
-    data() {
         return {
-            formData: emptyForm(),
-            responseMessage: '',
-            errors: {},
-            buttonText: "Submit",
-            submitted: false
-        }
-    },
-    methods: {
-        submitForm() {
-            fetch('email.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams(this.formData)
-            })
-            .then(res => res.json())
-            .then(data => {
-                if(data.errors) {
-                    this.errors = data.errors;
-                    this.responseMessage = '';
-                    return
-                }
-                //No Errors, or success state
-                this.errors = {};
-                this.responseMessage = data.message;
-                this.formData = emptyForm();
-                this.submitted = true;
-            })
-            .catch(error=>{
-                console.log(error);
-                this.errors = {
-                    general: "It seems you have lost your internet connection. Please try again later"
-                };
-                this.responseMessage = '';
-            })
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+            honeypot: ''
         }
     }
-})
-.mount("#app")
+
+    const app = Vue.createApp({
+        data() {
+            return {
+                formData: emptyForm(),
+                responseMessage: '',
+                errors: {},
+                buttonText: "Submit",
+                submitted: false
+            }
+        },
+        methods: {
+            submitForm() {
+                if (this.formData.honeypot) {
+                    this.submitted = true;
+                    return;
+                }
+                fetch('http://127.0.0.1:8000/api/contact', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: this.formData.name,
+                        email: this.formData.email,
+                        subject: this.formData.subject,
+                        message: this.formData.message
+                    })
+                })
+                .then(res => {
+                    if (!res.ok) {
+                        return res.json().then(data => {
+                            throw data;
+                        });
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    this.errors = {};
+                    this.responseMessage = 'Message sent successfully!';
+                    this.formData = emptyForm();
+                    this.submitted = true;
+                })
+                .catch(data => {
+                    if (data.errors) {
+                        this.errors = {};
+                        if (data.errors.name) this.errors.fullName = data.errors.name[0];
+                        if (data.errors.email) this.errors.email = data.errors.email[0];
+                        if (data.errors.subject) this.errors.subject = data.errors.subject[0];
+                        if (data.errors.message) this.errors.message = data.errors.message[0];
+                    } else {
+                        this.errors = {
+                            general: "Something went wrong with sending your message. Please try again later."
+                        };
+                    }
+                    this.responseMessage = '';
+                })
+            }
+        }
+    })
+    .mount("#contact-app")
 }
